d8e5947269ef55c75b30bf246dfd7c07ced00a46
teemvat <teemu.vataja@metropolia.fi>
2024-12-04 12:05:47 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
+import { useContext } from "react";
+import { AuthContext } from "./Context/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import RouteGuard from "./pages/RouteGuard";
+
+
+function Navigation() {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {isLoggedIn ? (
+          <>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Navigation />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/profile"
+              element={
+                <RouteGuard>
+                  <Profile />
+                </RouteGuard>
+              }
+            />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
